15a145a0f9244ed76260ffd20d0d06d434ae2f3b
서민혁 <mahns1201@gmail.com>
2023-10-30 02:53:57 +0900

    ADD: participant-detail style

--- client/src/pages/participant/participant-detail.jsx
+++ client/src/pages/participant/participant-detail.jsx
@@ -6,9 +6,13 @@ import { findParticipant } from "../../apis/participant.api";
 
 const ParticipantDetail = () => {
   const params = useParams();
-  const { channelId, participantId } = params;
+  const { participantId } = params;
 
   const navigate = useNavigate();
+
+  const onGoBack = () => {
+    navigate(-1);
+  };
 
   const [participant, setParticipant] = useState(0);
 
@@ -24,7 +28,31 @@ const ParticipantDetail = () => {
 
   return (
     <div id="participant-detail" className="contents">
-			<h1>{participant.name} 전적</h1>
+			<section className="participant-name">
+        <h1>{participant.name} 전적</h1>
+      </section>
+
+      <section className="home-button">
+        <button onClick={onGoBack}>↩︎</button>
+      </section>
+
+      <section className="total-record">
+        <h2>전체 전적</h2>
+        <div>
+          <h3>개별판</h3>
+          <span>{participant.winCount}승 {participant.looseCount}패 ({participant.winCount * 100 / (participant.winCount + participant.looseCount)}%)</span>
+        </div>
+        
+        <div>
+          <h3>다전제</h3>
+          <span>{participant.multiplePremisesWinCount}승 {participant.multiplePremisesLooseCount}패 ({participant.multiplePremisesWinCount * 100 / (participant.multiplePremisesWinCount + participant.multiplePremisesLooseCount)}%)</span>
+        </div>
+      </section>
+
+      <section className="relative-record">
+        <h2>상대 전적</h2>
+        준비중...
+      </section>
     </div>
   );
 };
